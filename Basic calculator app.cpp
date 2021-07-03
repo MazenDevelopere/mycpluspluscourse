4c3344dd24a15a7f97a681af9166f3231e5d160c
MazenDevelopere <48427322+MazenDevelopere@users.noreply.github.com>
2021-07-03 12:59:22 +0200

    2 Projects Uploaded
    For calculator app, You write a number, press enter, repeat, then you get the addition and subtraction result from the 2 numbers you wrote

---
+++ Basic calculator app.cpp
@@ -0,0 +1,25 @@
+// This is a basic program on C++
+//
+// Try to modify and run it and check out
+// the output in the terminal below.
+//
+// Happy coding! :-)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1;
+  int n2;
+  cin >> n1;
+  cin >> n2;
+  int addition = n1 + n2;
+  int subtraction = n1 - n2;
+  cout <<addition;
+  cout <<subtraction;
+  return {0};
+  
+    
+
+
+  }
---
+++ Hello World Project.cpp
@@ -0,0 +1,14 @@
+// This is a basic program on C++
+//
+// Try to modify and run it and check out
+// the output in the terminal below.
+//
+// Happy coding! :-)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout <<"Hello World!";
+    return 0;
+}
